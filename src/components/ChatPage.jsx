31811fe81660af43e1947e5902e4304e6966c110
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-05-10 14:36:28 +0200

    Add basic conversation state and mock data to ChatPage

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function ChatPage() {
+  const [step, setStep] = useState(0); // empezamos desde 0
+  // Simulamos una conversación para maquetar
+  const conversation = [
+    {
+      from: "user",
+      text: "Me preocupa que como humanidad no estemos a la altura del cambio que necesitamos.",
+    },
+    {
+      from: "bot",
+      text: "A veces no lo parece, pero hay información que nos dice que vamos por buen camino.",
+    },
+  ];
+
   return (
     <main className="chat">
       <section className="chat__container">
